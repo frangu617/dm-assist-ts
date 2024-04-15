8ca94707e6fcf46aed71747519c4f692ab2b8b02
Francisco E Gutierrez <142462584+frangu617@users.noreply.github.com>
2024-04-15 11:25:08 -0700

    fixed CharacterCreator.tsx

--- src/components/characters/CharacterCreator.tsx
+++ src/components/characters/CharacterCreator.tsx
@@ -1,16 +1,5 @@
 import { useState, useEffect } from "react";
-import {
-  Grid,
-  Button,
-  // Checkbox,
-  // FormControlLabel,
-  TextField,
-  Card,
-  CardContent,
-  // Tooltip,
-} from "@mui/material";
-// import AbilityScores from "./AbilityScore";
-// import Skills from './Skills';
+import { Grid, Button, TextField, Card, CardContent } from "@mui/material";
 import RaceSelector from "./RaceSelector";
 import AlignmentSelector from "./AlignmentSelector";
 import ClassSelector from "./ClassSelector";
@@ -18,10 +7,26 @@ import BackgroundSelector from "./BackgroundSelector";
 import Abilities from "./AbilitySelector";
 import SkillsSelector from "./SkillsSelector";
 
+interface Character {
+  name: string;
+  race: string;
+  class: string;
+  level: number;
+  alignment: string;
+  background: string;
+  abilities: { [key: string]: number };
+  skills: string[];
+  feats: string[];
+  hitPoints: number;
+  hitDice: string;
+  inventory: string[];
+  description: string;
+}
+
 export default function CharacterCreator() {
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState<Character[]>([]);
   const [resetKey, setResetKey] = useState(0);
-  const [newCharacter, setNewCharacter] = useState({
+  const [newCharacter, setNewCharacter] = useState<Character>({
     name: "",
     race: "",
     class: "",
@@ -40,82 +45,29 @@ export default function CharacterCreator() {
     feats: [],
     hitPoints: 10,
     hitDice: "1d10",
-    inventory: "",
-    description: "",
-  });
-  // Define arrays for races, classes, alignments, backgrounds, and skills
-  const skillsList = [
-    "Acrobatics",
-    "Animal Handling",
-    "Arcana",
-    "Athletics",
-    "Deception",
-    "History",
-    "Insight",
-    "Intimidation",
-    "Investigation",
-    "Medicine",
-    "Nature",
-    "Perception",
-    "Performance",
-    "Persuasion",
-    "Religion",
-    "Sleight of Hand",
-    "Stealth",
-    "Survival",
-  ];
-
-  const getInitialCharacterState = () => ({
-    name: "",
-    race: "",
-    class: "",
-    level: 1,
-    alignment: "",
-    background: "",
-    abilities: {
-      strength: 10,
-      dexterity: 10,
-      constitution: 10,
-      intelligence: 10,
-      wisdom: 10,
-      charisma: 10,
-    },
-    skills: [],
-    feats: [],
-    hitPoints: 10,
-    hitDice: "1d10",
-    inventory: "",
+    inventory: [],
     description: "",
   });
 
-  let VITE_APP_URL = import.meta.env.VITE_APP_URL;
-  console.log("VITE_API_URL: ", VITE_APP_URL);
   useEffect(() => {
     fetch(
-      `${import.meta.env.VITE_APP_URL}/api/characters` ||
+      `${process.env.VITE_APP_URL}/api/characters` ||
         "http://localhost:5000/api/characters"
     )
-      .then((response) => {
-        console.log("Response:", response);
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Data:", data);
-        setCharacters(data);
-      })
+      .then((response) => response.json())
+      .then((data: Character[]) => setCharacters(data))
       .catch((error) => console.error("Error fetching characters:", error));
   }, []);
 
-  const handleCharacterSubmit = (e) => {
+  const handleCharacterSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const characterData = {
+    const characterData: Character = {
       ...newCharacter,
-      inventory: newCharacter.inventory.split(",").map((item) => item.trim()), // Convert inventory string to array
+      inventory: newCharacter.inventory, // No need to split and trim here
     };
 
-    // Send a POST request to the server to create a character
-    fetch(`${import.meta.env.VITE_APP_URL}/api/characters`, {
+    fetch(`${process.env.VITE_APP_URL}/api/characters`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -123,36 +75,54 @@ export default function CharacterCreator() {
       body: JSON.stringify(characterData),
     })
       .then((response) => response.json())
-      .then((data) => {
+      .then((data: Character) => {
         setCharacters([...characters, data]);
-        setNewCharacter(getInitialCharacterState());
+        setNewCharacter({
+          ...newCharacter,
+          name: "",
+          race: "",
+          class: "",
+          level: 1,
+          alignment: "",
+          background: "",
+          abilities: {
+            strength: 10,
+            dexterity: 10,
+            constitution: 10,
+            intelligence: 10,
+            wisdom: 10,
+            charisma: 10,
+          },
+          skills: [],
+          feats: [],
+          hitPoints: 10,
+          hitDice: "1d10",
+          inventory: [],
+          description: "",
+        });
       })
       .catch((error) => console.error("Error creating character:", error));
 
-    // Reset form state
-    setNewCharacter(getInitialCharacterState());
-
-    // Increment key to force-reset child components like SkillsSelector
     setResetKey((prevKey) => prevKey + 1);
   };
 
-  const handleClassChange = (selectedClass) => {
-    setNewCharacter({ ...newCharacter, class: selectedClass }); // Update the selected class in the state
+  const handleClassChange = (selectedClass: string) => {
+    setNewCharacter({ ...newCharacter, class: selectedClass });
   };
 
-  const handleRaceChange = (selectedRace) => {
+  const handleRaceChange = (selectedRace: string) => {
     setNewCharacter({ ...newCharacter, race: selectedRace });
   };
 
-  const handleAlignmentChange = (selectedAlignment) => {
+  const handleAlignmentChange = (selectedAlignment: string) => {
     setNewCharacter({ ...newCharacter, alignment: selectedAlignment });
   };
 
-  const handleBackgroundChange = (selectedBackground) => {
+  const handleBackgroundChange = (selectedBackground: string) => {
     setNewCharacter({ ...newCharacter, background: selectedBackground });
   };
 
-  const handleAbilityChange = (ability, value) => {
+  const handleAbilityChange = (ability: string, value: number) => {
     setNewCharacter({
       ...newCharacter,
       abilities: {
@@ -162,25 +132,21 @@ export default function CharacterCreator() {
     });
   };
 
-  const handleSkillsChange = (selectedSkills) => {
-    // Update the character's skills based on selection from SkillsSelector
+  const handleSkillsChange = (selectedSkills: { name: string }[]) => {
     setNewCharacter({
       ...newCharacter,
-      skills: selectedSkills.map((skill) => skill.name), // Assuming selectedSkills is an array of skill objects
+      skills: selectedSkills.map((skill) => skill.name),
     });
   };
 
   return (
     <div>
       <h2>D&D Character Creator</h2>
-
-      {/* Character Creation Form */}
       <form onSubmit={handleCharacterSubmit}>
         <Card style={{ marginBottom: "20px" }}>
           <CardContent>
             <h3>Character Details:</h3>
             <Grid container spacing={2}>
-              {/* Name */}
               <Grid item xs={6} sm={4} lg={2}>
                 <TextField
                   fullWidth
@@ -192,33 +158,24 @@ export default function CharacterCreator() {
                   }
                 />
               </Grid>
-
-              {/* Race */}
               <Grid item xs={6} sm={4} lg={2}>
                 <RaceSelector
                   selectedRace={newCharacter.race}
                   onRaceChange={handleRaceChange}
                 />
               </Grid>
-
-              {/* Class */}
               <Grid item xs={6} sm={4} lg={2}>
                 <ClassSelector
                   selectedClass={newCharacter.class}
                   onClassChange={handleClassChange}
                 />
               </Grid>
-
-              {/* Alignment */}
-
               <Grid item xs={6} sm={4} lg={2}>
                 <AlignmentSelector
                   selectedAlignment={newCharacter.alignment}
                   onAlignmentChange={handleAlignmentChange}
                 />
               </Grid>
-
-              {/* Background */}
               <Grid item xs={6} sm={4} lg={2}>
                 <BackgroundSelector
                   selectedBackground={newCharacter.background}
@@ -228,8 +185,6 @@ export default function CharacterCreator() {
             </Grid>
           </CardContent>
         </Card>
-
-        {/* Abilities (Strength, Dexterity, etc.) */}
         <Card style={{ marginBottom: "20px" }}>
           <CardContent>
             <Abilities
@@ -238,7 +193,6 @@ export default function CharacterCreator() {
             />
           </CardContent>
         </Card>
-        {/* Skills */}
         <Card style={{ marginBottom: "20px" }}>
           <CardContent>
             <SkillsSelector
@@ -247,8 +201,6 @@ export default function CharacterCreator() {
             />
           </CardContent>
         </Card>
-
-        {/* Hit Points */}
         <Card style={{ marginBottom: "20px" }}>
           <CardContent>
             <Grid container spacing={2}>
@@ -267,8 +219,6 @@ export default function CharacterCreator() {
                   }
                 />
               </Grid>
-
-              {/* Hit Dice */}
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
@@ -283,8 +233,6 @@ export default function CharacterCreator() {
                   }
                 />
               </Grid>
-
-              {/* Inventory */}
               <Grid item xs={12}>
                 <TextField
                   fullWidth
@@ -292,16 +240,17 @@ export default function CharacterCreator() {
                   variant="outlined"
                   multiline
                   rows={3}
-                  value={newCharacter.inventory}
+                  value={newCharacter.inventory.join(", ")} // Convert array to string for display
                   onChange={(e) =>
                     setNewCharacter({
                       ...newCharacter,
-                      inventory: e.target.value,
+                      inventory: e.target.value
+                        .split(",")
+                        .map((item) => item.trim()),
                     })
                   }
                 />
               </Grid>
-              {/* Character Description */}
               <Grid item xs={12}>
                 <TextField
                   fullWidth
