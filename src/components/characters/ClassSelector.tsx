abb7657ee85396b126d4b95d5f3982f5be8b0044
Francisco E Gutierrez <142462584+frangu617@users.noreply.github.com>
2024-04-15 10:53:52 -0700

    fixed ClassSelector.tsx

--- src/components/characters/ClassSelector.tsx
+++ src/components/characters/ClassSelector.tsx
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Tooltip, TextField } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Tooltip, TextField } from "@mui/material";
 
-function ClassSelector({ selectedClass, onClassChange }) {
-    const [classes, setClasses] = useState([]);
-
-    const fetchClasses = () => {
-        fetch('https://www.dnd5eapi.co/api/classes')
-            .then((response) => response.json())
-            .then((data) => {
-                // Extract the race names from the fetched data
-                const classNames = data.results.map((clas) => clas.name);
-                setClasses(classNames); // Set the races state with the fetched data
-            })
-            .catch((error) => console.error('Error fetching races:', error));
-    };
-
-    useEffect(() => {
-        // Fetch races when the component mounts
-        fetchClasses();
-        // const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'];
-
-    })
-
-    return (
-        <Tooltip title={`Selected Class: ${selectedClass}`}>
-            <TextField
-                fullWidth
-                select
-                label="Class"
-                variant="outlined"
-                value={selectedClass}
-                onChange={(e) => onClassChange(e.target.value)}
-                SelectProps={{ native: true }}
-            >
-                <option value=""></option>
-                {classes.map((clas, index) => (
-                    <option key={index} value={clas}>
-                        {clas}
-                    </option>
-                ))}
-            </TextField>
-        </Tooltip>
-    )
+interface ClassSelectorProps {
+  selectedClass: string;
+  onClassChange: (selectedClass: string) => void;
 }
 
-export default ClassSelector
+const ClassSelector: React.FC<ClassSelectorProps> = ({
+  selectedClass,
+  onClassChange,
+}) => {
+  const [classes, setClasses] = useState<string[]>([]);
+
+  const fetchClasses = () => {
+    fetch("https://www.dnd5eapi.co/api/classes")
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the class names from the fetched data
+        const classNames = data.results.map(
+          (clas: { name: string }) => clas.name
+        );
+        setClasses(classNames); // Set the classes state with the fetched data
+      })
+      .catch((error) => console.error("Error fetching classes:", error));
+  };
+
+  useEffect(() => {
+    // Fetch classes when the component mounts
+    fetchClasses();
+    // const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'];
+  }, []);
+
+  return (
+    <Tooltip title={`Selected Class: ${selectedClass}`}>
+      <TextField
+        fullWidth
+        select
+        label="Class"
+        variant="outlined"
+        value={selectedClass}
+        onChange={(e) => onClassChange(e.target.value)}
+        SelectProps={{ native: true }}
+      >
+        <option value=""></option>
+        {classes.map((clas, index) => (
+          <option key={index} value={clas}>
+            {clas}
+          </option>
+        ))}
+      </TextField>
+    </Tooltip>
+  );
+};
+
+export default ClassSelector;
