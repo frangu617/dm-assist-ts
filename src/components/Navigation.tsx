4097c6b68b651415ffcd9688ef9eab52e7b2c188
frangu617 <142462584+frangu617@users.noreply.github.com>
2024-04-22 21:28:25 -0700

    fixing some styling, and trying to improve chat

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,201 +1,174 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "./contexts/CurrentUser"; // Adjust the import path as necessary
+import { useCurrentUser } from "./contexts/CurrentUser";
 import {
   Typography,
   Button,
-  Menu,
-  MenuItem,
-  Toolbar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
   AppBar,
+  Toolbar,
+  IconButton,
 } from "@mui/material";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navigation: React.FC = () => {
   const { currentUser } = useCurrentUser();
-  // const [username, setUsername] = useState<string | null>(null);
-  const [anchorElCharacter, setAnchorElCharacter] =
-    useState<null | HTMLElement>(null);
-  const [anchorElTools, setAnchorElTools] = useState<null | HTMLElement>(null);
-  const [anchorElReference, setAnchorElReference] =
-    useState<null | HTMLElement>(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  // const handleLoginSuccess = (username: string, token: string) => {
-  //   localStorage.setItem("token", token);
-  //   setUsername(username);
-  //   // const decoded = jwtDecode(token); // Assuming your JWT contains the user ID
-  //   // setCurrentUserId(username);
-  // };
-
-  const handleOpenMenu = (
-    event: React.MouseEvent<HTMLElement>,
-    menuType: string
-  ) => {
-    switch (menuType) {
-      case "Character":
-        setAnchorElCharacter(event.currentTarget);
-        break;
-      case "Tools":
-        setAnchorElTools(event.currentTarget);
-        break;
-      case "Reference":
-        setAnchorElReference(event.currentTarget);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleCloseMenu = (menuType: string) => {
-    switch (menuType) {
-      case "Character":
-        setAnchorElCharacter(null);
-        break;
-      case "Tools":
-        setAnchorElTools(null);
-        break;
-      case "Reference":
-        setAnchorElReference(null);
-        break;
-      default:
-        break;
-    }
-  };
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setDrawerOpen(open);
+    };
 
   return (
     <nav>
-      {currentUser ? (
-        // If logged in, show these links
-        <>
-          <AppBar position="static" style={{ marginBottom: "20px" }}>
-      <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
-        
-
-        <Button
-          color="inherit"
-          aria-haspopup="true"
-          onClick={(event) => handleOpenMenu(event, 'Character')}
-          endIcon={<ArrowDropDownIcon />}
-        >
-          Character
-        </Button>
-        <Menu
-          id="character-menu"
-          anchorEl={anchorElCharacter}
-          open={Boolean(anchorElCharacter)}
-          onClose={() => handleCloseMenu('Character')}
-        >
-          <MenuItem onClick={() => handleCloseMenu('Character')}>
-            <NavLink to="/create" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Character Creator
-            </NavLink>
-          </MenuItem>
-          <MenuItem onClick={() => handleCloseMenu('Character')}>
-            <NavLink to="/manager" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Character Manager
-            </NavLink>
-          </MenuItem>
-        </Menu>
-
-        <Button
-          color="inherit"
-          aria-haspopup="true"
-          onClick={(event) => handleOpenMenu(event, 'Tools')}
-          endIcon={<ArrowDropDownIcon />}
-        >
-          Tools
-        </Button>
-        <Menu
-          id="tools-menu"
-          anchorEl={anchorElTools}
-          open={Boolean(anchorElTools)}
-          onClose={() => handleCloseMenu('Tools')}
-        >
-          <MenuItem onClick={() => handleCloseMenu('Tools')}>
-            <NavLink to="/dice" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Dice Roller
-            </NavLink>
-          </MenuItem>
-          <MenuItem onClick={() => handleCloseMenu('Tools')}>
-            <NavLink to="/initiative-tracker" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Initiative Tracker
-            </NavLink>
-          </MenuItem>
-          <MenuItem onClick={() => handleCloseMenu('Tools')}>
-            <NavLink to="/music-search" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Music Search
-            </NavLink>
-          </MenuItem>
-        </Menu>
-
-        <Button
-          color="inherit"
-          aria-haspopup="true"
-          onClick={(event) => handleOpenMenu(event, 'Reference')}
-          endIcon={<ArrowDropDownIcon />}
-        >
-          Reference
-        </Button>
-        <Menu
-          id="reference-menu"
-          anchorEl={anchorElReference}
-          open={Boolean(anchorElReference)}
-          onClose={() => handleCloseMenu('Reference')}
-        >
-          <MenuItem onClick={() => handleCloseMenu('Reference')}>
-            <NavLink to="/monster-search" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Monster Search
-            </NavLink>
-          </MenuItem>
-          <MenuItem onClick={() => handleCloseMenu('Reference')}>
-            <NavLink to="/races-search" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Races Search
-            </NavLink>
-          </MenuItem>
-          <MenuItem onClick={() => handleCloseMenu('Reference')}>
-            <NavLink to="/rules-search" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Rules Search
-            </NavLink>
-          </MenuItem>
-          <MenuItem onClick={() => handleCloseMenu('Reference')}>
-            <NavLink to="/classes" style={{ textDecoration: 'none', color: 'inherit' }}>
-              Classes
-            </NavLink>
-          </MenuItem>
-        </Menu>
-
-        <Typography variant="h6" style={{ marginLeft: "12px" }}>
-          Welcome, {currentUser.username}!
-        </Typography>
-
-        <Button color="inherit">
-          <NavLink to="/logout" style={{ textDecoration: 'none', color: 'inherit' }}>
-            Logout
-          </NavLink>
-        </Button>
-      </Toolbar>
-    </AppBar>
-        </>
-      ) : (
-        <>
-          <Button color="inherit">
+      <AppBar
+        position="fixed"
+        style={{ marginBottom: "20px", boxShadow: "none" }}
+      >
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
             <NavLink
-              to="/login"
+              to="/"
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              Login
+              🐉DM Assist🐉{" "}
             </NavLink>
-          </Button>
-          <Button color="inherit">
-            <NavLink
-              to="/signup"
-              style={{ textDecoration: "none", color: "inherit" }}
+          </Typography>
+          {currentUser ? (
+            <Typography variant="h6" style={{ marginRight: "12px" }}>
+              DM, {currentUser.username}!
+            </Typography>
+          ) : (
+            <>
+              <Button
+                color="inherit"
+                style={{ marginRight: "12px", boxShadow: "1px 1px 0 #000" }}
+              >
+                <NavLink
+                  to="/login"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Login
+                </NavLink>
+              </Button>
+              <Button color="inherit"
+              style={{ marginRight: "12px", boxShadow: "1px 1px 0 #000" }}>
+                <NavLink
+                  to="/signup"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Register
+                </NavLink>
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List>
+          <ListItem
+            button
+            component={NavLink}
+            to="/create"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Character Creator" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/manager"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Character Manager" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/dice"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Dice Roller" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/initiative-tracker"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Initiative Tracker" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/music-search"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Music Search" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/monster-search"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Monster Search" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/races-search"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Races Search" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/rules-search"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Rules Search" />
+          </ListItem>
+          <ListItem
+            button
+            component={NavLink}
+            to="/classes"
+            onClick={toggleDrawer(false)}
+          >
+            <ListItemText primary="Classes" />
+          </ListItem>
+          {currentUser && (
+            <ListItem
+              button
+              component={NavLink}
+              to="/logout"
+              onClick={toggleDrawer(false)}
             >
-              Sign Up
-            </NavLink>
-          </Button>
-        </>
-      )}
+              <ListItemText primary="Logout" />
+            </ListItem>
+          )}
+        </List>
+      </Drawer>
     </nav>
   );
 };
